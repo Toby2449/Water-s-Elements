a07bbd4edaea8f63bb95e43af1a6e3ced5658735
Toby <tobytimm75@gmail.com>
2018-08-31 03:02:24 -0500

    Added hold alt for stats

--- src/main/java/com/water/elementmod/init/EmItems.java
+++ src/main/java/com/water/elementmod/init/EmItems.java
@@ -24,11 +24,11 @@ public class EmItems
 	public static final ToolMaterial MATERIAL_FIRE4 = EnumHelper.addToolMaterial("material_fire4", 3, 1561, 8.0F, 5.0F, 10);
 	public static final ToolMaterial MATERIAL_FIRE5 = EnumHelper.addToolMaterial("material_fire5", 3, 1561, 8.0F, 5.0F, 10);
 	
-	public static final ItemSword TEST_SWORD = new FireSword("test_sword", 1, MATERIAL_FIRE1);
-	public static final ItemSword TEST2_SWORD = new FireSword("test2_sword", 2, MATERIAL_FIRE2);
-	public static final ItemSword TEST3_SWORD = new FireSword("test3_sword", 3, MATERIAL_FIRE3);
-	public static final ItemSword TEST4_SWORD = new FireSword("test4_sword", 4, MATERIAL_FIRE4);
-	public static final ItemSword TEST5_SWORD = new FireSword("test5_sword", 5, MATERIAL_FIRE5);
+	public static final ItemSword FIRE_SWORD1 = new FireSword("fire_sword1", 1, MATERIAL_FIRE1);
+	public static final ItemSword FIRE_SWORD2 = new FireSword("fire_sword2", 2, MATERIAL_FIRE2);
+	public static final ItemSword FIRE_SWORD3 = new FireSword("fire_sword3", 3, MATERIAL_FIRE3);
+	public static final ItemSword FIRE_SWORD4 = new FireSword("fire_sword4", 4, MATERIAL_FIRE4);
+	public static final ItemSword FIRE_SWORD5 = new FireSword("fire_sword5", 5, MATERIAL_FIRE5);
 	
 	public static final Item VOIDESS = new ItemBaseShiny("item_void_essence", 16);
 	public static final Item VOIDSING = new ItemBaseShiny("item_void_singularity", 1);
--- src/main/java/com/water/elementmod/items/tools/FireSword.java
+++ src/main/java/com/water/elementmod/items/tools/FireSword.java
@@ -9,6 +9,9 @@ import com.water.elementmod.Main;
 import com.water.elementmod.init.EmItems;
 import com.water.elementmod.util.IHasModel;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.renderer.RenderGlobal;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.Entity;
@@ -23,12 +26,14 @@ import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.world.World;
+import net.minecraftforge.common.DimensionManager;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class FireSword extends ItemSword implements IHasModel
 {
 	private int level;
+	private ToolMaterial material;
 	
 	public FireSword(String name, Integer level, ToolMaterial material) 
 	{
@@ -38,12 +43,13 @@ public class FireSword extends ItemSword implements IHasModel
 		setCreativeTab(Main.tab_everything);
 		
 		this.level = level;
+		this.material = material;
 		EmItems.ITEMS.add(this);
 	}
 	
-	public String intToNumeral(Integer level)
+	public String intToNumeral()
 	{
-		switch(level)
+		switch(this.level)
 		{
 		case 1:
 			return "I";
@@ -67,50 +73,90 @@ public class FireSword extends ItemSword implements IHasModel
 		return true;
 	}
 	
+	public int getAddedDamage() {
+		return (int)this.material.getAttackDamage() - (int)ToolMaterial.DIAMOND.getAttackDamage();
+	}
+	
+	public int getFireDuration(Boolean isRandom, Boolean isMax)
+	{
+		int Range = 2;
+		Random rand = new Random();
+		if(!isMax) 
+		{
+			switch(this.level)
+			{
+			case 1:
+				if(!isRandom) return 2;
+				return 2 + rand.nextInt(Range + 1);
+			case 2:
+				if(!isRandom) return 3;
+				return 3 + rand.nextInt(Range + 1);
+			case 3:
+				if(!isRandom) return 4;
+				return 4 + rand.nextInt(Range + 1);
+			case 4:
+				if(!isRandom) return 5;
+				return 5 + rand.nextInt(Range + 1);
+			case 5:
+				if(!isRandom) return 6;
+				return 6 + rand.nextInt(Range + 1); // Have to add +1 because it goes from 0-1 (which is 2 numbers)
+			}
+		}
+		return Range;
+	}
+	
 	@Override
 	public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> list, ITooltipFlag flagIn)
 	{
 		list.add(I18n.format("tooltip.FireEnchant"));
-	    list.add(I18n.format("tooltip.EnchantFireLevel") + " " + intToNumeral(this.level) + I18n.format("tooltip.ResetFormatting"));
-	    list.add(" ");
+	    list.add(I18n.format("tooltip.EnchantFireLevel") + " " + intToNumeral() + I18n.format("tooltip.ResetFormatting"));
+	    
+	    if(GuiScreen.isAltKeyDown()){
+	    	//list.remove(I18n.format("tooltip.PressAlt") + I18n.format("tooltip.ResetFormatting"));
+	    	list.add("");
+	    	list.add(I18n.format("tooltip.2xDamageNature"));
+	    	list.add(I18n.format("tooltip.FireFormatting") + "+" + this.getAddedDamage() + " " + I18n.format("tooltip.MoreAttackDamage") + I18n.format("tooltip.ResetFormatting"));
+	    	list.add(I18n.format("tooltip.FireDuration") + this.getFireDuration(false, false) + "-" + (this.getFireDuration(false, false) + this.getFireDuration(false, true) ) + "s" + I18n.format("tooltip.ResetFormatting"));
+	    } else {
+	    	list.add(I18n.format("tooltip.PressAlt") + I18n.format("tooltip.ResetFormatting"));
+	    }
+	    
+	    list.add(""); // Works the same way as \n
 	}
 	
 	@Override
-
 	public boolean hitEntity(ItemStack stack, EntityLivingBase target, EntityLivingBase attacker)
 	{
-		switch(this.level)
-		{
-		case 1:
-			target.setFire(2);
-			break;
-		case 2:
-			target.setFire(3);
-			break;
-		case 3:
-			target.setFire(3);
-			break;
-		case 4:
-			target.setFire(4);
-			break;
-		case 5:
-			target.setFire(5);
-			break;
-		}
-	    stack.damageItem(1, attacker);
+		target.setFire(getFireDuration(true, false));
+		stack.damageItem(1, attacker);
+		FireParticleEffect(target);
 	    return true;
 	}
 	
+	public boolean FireParticleEffect(EntityLivingBase target)
+	{
+		World world = Minecraft.getMinecraft().world;
+		if(world == null) return false;
+		for(int countparticles = 0; countparticles <= 7 * this.level; ++countparticles)
+		{
+			Random rand = new Random();
+			world.spawnParticle(EnumParticleTypes.FLAME, target.posX + (rand.nextDouble() - 0.5D) * (double)target.width, target.posY + rand.nextDouble() * (double)target.height - (double)target.getYOffset(), target.posZ + (rand.nextDouble() - 0.5D) * (double)target.width, 0.0D, 0.0D,0.0D);
+		}
+		for(int countparticles = 0; countparticles <= 10 * this.level; ++countparticles)
+		{
+			Random rand = new Random();
+			world.spawnParticle(EnumParticleTypes.LAVA, target.posX + (rand.nextDouble() - 0.5D) * (double)target.width, target.posY + rand.nextDouble() * (double)target.height - (double)target.getYOffset(), target.posZ + (rand.nextDouble() - 0.5D) * (double)target.width, 0.0D, 0.0D,0.0D);
+		}
+		return true;
+	}
+	
 	@Override
-	public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn)
+	public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer target, EnumHand handIn)
     {
 		Random rand = new Random();
-		for(int countparticles = 0; countparticles <= 10; ++countparticles)
-		{
-			worldIn.spawnParticle(EnumParticleTypes.FLAME, playerIn.posX + 1.0D * (double)playerIn.width, playerIn.posY + rand.nextDouble() * (double)playerIn.height - (double)playerIn.getYOffset(), playerIn.posZ + 1.0D, 0.0D, 0.0D, 0.0D);
-		}
+		target.getEntityWorld().spawnParticle(EnumParticleTypes.FLAME, target.posX + (rand.nextDouble() - 0.5D) * (double)target.width, target.posY + rand.nextDouble() * (double)target.height - (double)target.getYOffset(), target.posZ + (rand.nextDouble() - 0.5D) * (double)target.width, 0.0D, 0.0D,0.0D);
 		
-		return new ActionResult<ItemStack>(EnumActionResult.PASS, playerIn.getHeldItem(handIn));
+		return new ActionResult<ItemStack>(EnumActionResult.PASS, target.getHeldItem(handIn));
 	}
 	
 	@Override
